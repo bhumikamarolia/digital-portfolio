273115601aab727ff84714ef6b7f78ff9220ee45
unknown <maroliabhumika@gmail.com>
2021-02-03 11:00:20 +0530

    initial commit

---
+++ profile-frontend/src/data/config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  // TODO: change your profile information here
+  name: "Bhumika Marolia",
+  greeting: "Hey 👋",
+  greetingDescription: "I'm Bhumika Marolia and I'm a Software Engineer!",
+  githubUrl: "https://github.com/bhumikamarolia",
+  linkedinUrl: "https://linkedin.com/in/bhumikamarolia",
+  cvLink: "https://drive.google.com/file/d/1opHBAg0-pIlHARMtjgCRITBPtPXaI9vw/view?usp=drivesdk",
+};
